582189eb154f22145ba044236434d490d644cf70
NishaVijayamma <vsn4891@gmail.com>
2025-11-25 10:51:22 +0100

    tricentris

---
+++ src/test/java/TricentisTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TricentisTest {
+    @Test
+    public void tricentisOpenTest(){
+         WebDriver driver = new ChromeDriver();
+         // open web browser
+        driver.get("https://demowebshop.tricentis.com/");
+        //System.out.println(driver.getTitle());
+      //  assertEquals("Demo Web Shop",driver.getTitle());
+        WebElement searchBox = driver.findElement(By.id("small-searchterms"));
+        driver.quit();
+    }
+}
